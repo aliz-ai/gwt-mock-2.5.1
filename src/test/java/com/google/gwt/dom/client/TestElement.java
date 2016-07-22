764383e5dedafae966ca3e2eabefde6219233ab5
gabor.farkas@doctusoft.com <gabor.farkas@doctusoft.com>
2016-07-22 14:00:05 +0200

    fixed compilation for java 7, dumped version

--- src/test/java/com/google/gwt/dom/client/TestElement.java
+++ src/test/java/com/google/gwt/dom/client/TestElement.java
@@ -11,12 +11,12 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class TestElement {
-    
-    @Before
-    public void setup() {
-        GWTMock.reset();
-    }
-    
+	
+	@Before
+	public void setup() {
+		GWTMock.reset();
+	}
+	
 	@Test
 	public void testInnerHTMLSimpleText() {
 		Label label = new Label("hello world");
@@ -29,20 +29,20 @@ public class TestElement {
 		Assert.assertEquals("changed inner html", element.getInnerText());
 		Assert.assertTrue(Document.get().getBody().getInnerHTML().contains("changed inner html"));
 		// IsEqualIgnoringWhiteSpace
-	    Assert.assertEquals("<div className=\"gwt-Label\">changed inner html</div>", Document.get().getBody().getInnerHTML().replaceAll("\\R", ""));
+		Assert.assertEquals("<div className=\"gwt-Label\">changed inner html</div>", Document.get().getBody().getInnerHTML().replaceAll("[\\r\\n]", ""));
 	}
 	
 	@Test
-    public void testInnerHTMLOfComposite() {
-	    FlowPanel composite = new FlowPanel();
-	    composite.add(new Label("one"));
-	    composite.add(new Label("two"));
-        RootPanel.get().add(composite);
-
-        // IsEqualIgnoringWhiteSpace
-        Assert.assertEquals("<div>  <div className=\"gwt-Label\">one  </div>" + //
-                "  <div className=\"gwt-Label\">two  </div></div>", Document.get().getBody().getInnerHTML().replaceAll("\\R", ""));
-    }
+	public void testInnerHTMLOfComposite() {
+		FlowPanel composite = new FlowPanel();
+		composite.add(new Label("one"));
+		composite.add(new Label("two"));
+		RootPanel.get().add(composite);
+		
+		// IsEqualIgnoringWhiteSpace
+		Assert.assertEquals("<div>  <div className=\"gwt-Label\">one  </div>" + //
+				"  <div className=\"gwt-Label\">two  </div></div>", Document.get().getBody().getInnerHTML().replaceAll("[\\r\\n]", ""));
+	}
 	
 	@Test
 	public void testInnerHTMLWithInnerElement() {
@@ -53,27 +53,27 @@ public class TestElement {
 		Assert.assertEquals("hello world", element.getInnerText());
 		element.setInnerHTML("changed <div>inner</div> html");
 		Assert.assertEquals("fragments converted to child nodes", 3, element.getChildCount()); // verified in GWT
-		Assert.assertEquals("changed <div>inner</div> html", element.getInnerHTML().replaceAll("\\R", ""));
+		Assert.assertEquals("changed <div>inner</div> html", element.getInnerHTML().replaceAll("[\\r\\n]", ""));
 	}
 	
 	@Test
-    public void testInnerHTMLWithComplexInnerElements() {
-        Label label = new Label();
-        Element element = label.getElement();
-        RootPanel.get().add(label);
-        element.setInnerHTML("L1.1<div>L2.1<div>L3.1</div>L2.2<div>L3.2</div>L2.3</div>L1.2");
-        Assert.assertEquals("fragments converted to child nodes", 3, element.getChildCount()); 
-        Assert.assertEquals("fragments converted to child nodes", 5, element.getChild(1).getChildCount()); 
-        Assert.assertEquals("L1.1<div>L2.1  <div>L3.1  </div>L2.2  <div>L3.2  </div>L2.3</div>L1.2", element.getInnerHTML().replaceAll("\\R", ""));
-    }
+	public void testInnerHTMLWithComplexInnerElements() {
+		Label label = new Label();
+		Element element = label.getElement();
+		RootPanel.get().add(label);
+		element.setInnerHTML("L1.1<div>L2.1<div>L3.1</div>L2.2<div>L3.2</div>L2.3</div>L1.2");
+		Assert.assertEquals("fragments converted to child nodes", 3, element.getChildCount());
+		Assert.assertEquals("fragments converted to child nodes", 5, element.getChild(1).getChildCount());
+		Assert.assertEquals("L1.1<div>L2.1  <div>L3.1  </div>L2.2  <div>L3.2  </div>L2.3</div>L1.2", element.getInnerHTML().replaceAll("[\\r\\n]", ""));
+	}
 	
-    @Test
-    public void testToString() {
-        Label label = new Label("hello world");
-        Element element = label.getElement();
-        RootPanel.get().add(label);
-
-        Assert.assertEquals("hello world", element.getInnerHTML());
-        Assert.assertEquals("<div className=\"gwt-Label\">hello world</div>", element.toString().replaceAll("\\R", ""));
-    }
+	@Test
+	public void testToString() {
+		Label label = new Label("hello world");
+		Element element = label.getElement();
+		RootPanel.get().add(label);
+		
+		Assert.assertEquals("hello world", element.getInnerHTML());
+		Assert.assertEquals("<div className=\"gwt-Label\">hello world</div>", element.toString().replaceAll("[\\r\\n]", ""));
+	}
 }
